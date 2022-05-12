b11945902a0c7def4687fefa8d42652f5c655472
MagicMoleMM <101579330+MagicMoleMM@users.noreply.github.com>
2022-05-13 00:07:23 +0300

    Update tg_send_message.py

--- tg_send_message.py
+++ tg_send_message.py
@@ -3,7 +3,7 @@ import requests
 
 def telegram_bot_sendtext(bot_message):
     
-    bot_token = '5391670074:AAFAYmmkvPZhjBuQemh35pI7UHQtcHbniiU'
+    bot_token = '5391670074:AAFAYmmkvPZhjBuQemh35pI7UHQtcHbnbla'
     bot_chatID = '@TradingSignals_MagicMole'
 
     send_text = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={bot_chatID}&parse_mode=Markdown&text={bot_message}'
